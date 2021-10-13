cc7256650b77d307c6f63fa90aebe996cf80312a
Vench_97 <vnuk-vanya@mail.ru>
2021-10-13 15:02:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+a = 7
+b = 12
+print(a < 10 and b > 9)
+print(a > 4 and b < 15)
+print(a <= 5 and b >= 11)
+print(a == 12 and b != 7)
+print(a < 10 or b > 9)
+print(a > 4 or b < 15)
+print(a < 5 or b == 11)
+print(a !=7 or b > 18)
+x = 'Hello friend'
+y = 'What is your name'
+print('H' in x and 'W' in y)
+print('ll' in x and 'is' in y)
+print('are' in x or 'he' in y )
+print('sun' in x or 'q' in y)
